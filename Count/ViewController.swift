8bb6d59baf429a9db0bffa85ecfdbb0190c4238c
obaya884 <taku3.884@gmail.com>
2020-05-07 12:04:07 +0900

    Initial Commit

---
+++ Count/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by 大林拓実 on 2020/05/07.
+//  Copyright © 2020 Obayashi Takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
