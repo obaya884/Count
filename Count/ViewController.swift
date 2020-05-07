489581a2f69e9d20b07bef81968a3a15fc8ebbdb
obaya884 <taku3.884@gmail.com>
2020-05-07 12:17:07 +0900

    add plus func

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var countLabel: UILabel!
+    @IBOutlet var plusButton: UIButton!
+    
+    var count: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countLabel.text = "0"
+        plusButton.layer.cornerRadius = 50
     }
 
+    @IBAction func plus() {
+        count += 1
+        countLabel.text = String(count)
+    }
 
 }
 
